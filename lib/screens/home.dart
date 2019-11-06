b4962dde99e2d0404c4beb08d33352267ed6a8d5
pakk <mr.tu2non@gmail.com>
2019-11-06 15:21:30 +0700

    design ui is completed

---
+++ lib/screens/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:pakk/utility/my_style.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+// field
+
+// Method
+
+  Widget signInbutton() {
+    return Expanded(
+      child: RaisedButton(
+        shape: MyStyle().curveButton,
+        color: MyStyle().textColor,
+        child: Text(
+          'Sign in',
+          style: MyStyle().myWhiteTextStyle,
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpbutton() {
+    return Expanded(
+      child: OutlineButton(
+        shape: MyStyle().curveButton,
+        borderSide: BorderSide(color: MyStyle().textColor),
+        child: Text('Sign Up', style: TextStyle(color: MyStyle().textColor)),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 200.0,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          signInbutton(),
+          SizedBox(
+            width: 4.0,
+          ),
+          signUpbutton(),
+        ],
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 200.0,
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'User:'),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 200.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Pass:'),
+      ),
+    );
+  }
+
+  Widget showlogo() {
+    return Container(
+      width: 150.0,
+      height: 150.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Pakk Pipestate',
+      style: TextStyle(
+        fontSize: MyStyle().h1,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: MyStyle().textColor,
+        fontFamily: 'Pattaya',
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/wallpaper.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showlogo(),
+              showAppName(),
+              userText(),
+              passwordText(),
+              showButton(),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class MyStyle {
+
+  double h1 = 30.0, h2 = 18.0;
+  Color textColor = Colors.red[400];
+  Color mainColor = Colors.orangeAccent[400];
+  String nameFont = 'Pattaya';
+  String nameApp = 'pakk';
+  
+  TextStyle myWhiteTextStyle = TextStyle(color: Colors.white);
+
+  ShapeBorder curveButton = RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0));
+
+  MyStyle();
+  
+}
