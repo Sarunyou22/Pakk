f5fd5a92ba5bfb86f3e553e7f42a56484a37669a
pakk <mr.tu2non@gmail.com>
2019-11-07 15:03:13 +0700

    post data completed

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:pakk/utility/my_style.dart';
 import 'package:intl/intl.dart';
 
@@ -9,6 +13,8 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   // Field
+  final formKey = GlobalKey<FormState>();
+  String name, user, password, dateString;
 
   // Method
   Widget nameText() {
@@ -27,6 +33,16 @@ class _RegisterState extends State<Register> {
           helperStyle: TextStyle(color: Colors.lightBlueAccent),
           hintText: 'English Only',
         ),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Please Fill Name in the Blank';
+          } else {
+            return null;
+          }
+        },
+        onSaved: (value) {
+          name = value.trim();
+        },
       ),
     );
   }
@@ -35,6 +51,7 @@ class _RegisterState extends State<Register> {
     return Container(
       margin: EdgeInsets.only(left: 30.0, right: 30.0),
       child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
           icon: Icon(
             Icons.account_box,
@@ -45,8 +62,20 @@ class _RegisterState extends State<Register> {
           labelStyle: TextStyle(color: Colors.cyanAccent),
           helperText: 'Type You User',
           helperStyle: TextStyle(color: Colors.cyanAccent),
-          hintText: 'English Only',
+          hintText: 'YourEmail@email.com',
         ),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Please fill Username';
+          } else if (!((value.contains('@')) && (value.contains('.')))) {
+            return 'Please Keep Email Format';
+          } else {
+            return null;
+          }
+        },
+        onSaved: (value) {
+          user = value.trim();
+        },
       ),
     );
   }
@@ -66,8 +95,18 @@ class _RegisterState extends State<Register> {
           labelStyle: TextStyle(color: Colors.yellow),
           helperText: 'Type You Password',
           helperStyle: TextStyle(color: Colors.yellowAccent),
-          hintText: 'English Only',
+          hintText: 'More 6 Charctor',
         ),
+        validator: (value) {
+          if (value.length < 6) {
+            return 'Password More 6 Charactor';
+          } else {
+            return null;
+          }
+        },
+        onSaved: (value) {
+          password = value.trim();
+        },
       ),
     );
   }
@@ -76,7 +115,7 @@ class _RegisterState extends State<Register> {
     DateTime dateTime = DateTime.now();
     print('DateTime = $dateTime');
 
-    String dateString = DateFormat('yyyy-mm-dd').format(dateTime);
+    dateString = DateFormat('yyyy-mm-dd').format(dateTime);
     print('DateTime = $dateString');
 
     return Text(
@@ -89,8 +128,29 @@ class _RegisterState extends State<Register> {
     return IconButton(
       tooltip: 'Upload Now',
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        print('You Click Register');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('name = $name, user = $user, password = $password');
+          registerThread();
+        }
+      },
     );
+  }
+
+  Future<void> registerThread() async {
+    String url ='https://www.androidthai.in.th/pint/addUserPakk.php?isAdd=true&Name=$name&User=$user&Password=$password&RegisDate=$dateString';
+        
+    Response response = await get(url);
+    print('response = $response');
+    var result = json.decode(response.body);
+    print('result = $result');
+    if (result.toString() == 'true') {
+      Navigator.of(context).pop();
+      
+    } else {
+    }
   }
 
   @override
@@ -104,22 +164,25 @@ class _RegisterState extends State<Register> {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.white, Colors.yellowAccent[100], MyStyle().mainColor],
+            colors: [Colors.white, Colors.white, MyStyle().mainColor],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
-        child: ListView(
-          padding: EdgeInsets.all(30.0),
-          children: <Widget>[
-            showCurrentDate(),
-            SizedBox(
-              height: 20.0,
-            ),
-            nameText(),
-            userText(),
-            passwordText(),
-          ],
+        child: Form(
+          key: formKey,
+          child: ListView(
+            padding: EdgeInsets.all(30.0),
+            children: <Widget>[
+              showCurrentDate(),
+              SizedBox(
+                height: 20.0,
+              ),
+              nameText(),
+              userText(),
+              passwordText(),
+            ],
+          ),
         ),
       ),
     );
