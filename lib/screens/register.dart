fa0ca90c90de319aae10d9aca0167ff332fae1a7
pakk <mr.tu2non@gmail.com>
2019-11-07 11:32:21 +0700

    design interface Register  completed

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pakk/utility/my_style.dart';
+import 'package:intl/intl.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -6,13 +8,120 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Field
+
+  // Method
+  Widget nameText() {
+    return Container(
+      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.folder_open,
+            size: 36.0,
+            color: Colors.greenAccent,
+          ),
+          labelText: 'Display Name',
+          labelStyle: TextStyle(color: Colors.indigo),
+          helperText: 'Type You First Name',
+          helperStyle: TextStyle(color: Colors.lightBlueAccent),
+          hintText: 'English Only',
+        ),
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.account_box,
+            size: 36.0,
+            color: Colors.cyanAccent,
+          ),
+          labelText: 'User',
+          labelStyle: TextStyle(color: Colors.cyanAccent),
+          helperText: 'Type You User',
+          helperStyle: TextStyle(color: Colors.cyanAccent),
+          hintText: 'English Only',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            size: 36.0,
+            color: Colors.greenAccent,
+          ),
+          labelText: 'Password',
+          labelStyle: TextStyle(color: Colors.yellow),
+          helperText: 'Type You Password',
+          helperStyle: TextStyle(color: Colors.yellowAccent),
+          hintText: 'English Only',
+        ),
+      ),
+    );
+  }
+
+  Widget showCurrentDate() {
+    DateTime dateTime = DateTime.now();
+    print('DateTime = $dateTime');
+
+    String dateString = DateFormat('yyyy-mm-dd').format(dateTime);
+    print('DateTime = $dateString');
+
+    return Text(
+      'Date = $dateString',
+      style: MyStyle().myTextStyleH2,
+    );
+  }
+
+  Widget registerButton() {
+    return IconButton(
+      tooltip: 'Upload Now',
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: MyStyle().textColor,
         title: Text('Register'),
+        actions: <Widget>[registerButton()],
       ),
-      body: Text('body'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.yellowAccent[100], MyStyle().mainColor],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            showCurrentDate(),
+            SizedBox(
+              height: 20.0,
+            ),
+            nameText(),
+            userText(),
+            passwordText(),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
-
-
 class MyStyle {
-
   double h1 = 30.0, h2 = 18.0;
   Color textColor = Colors.red[400];
-  Color mainColor = Colors.orangeAccent[400];
+  Color mainColor = Colors.blue[200];
   String nameFont = 'Pattaya';
   String nameApp = 'pakk';
-  
+
   EdgeInsetsGeometry myPadding = EdgeInsets.all(20.0);
 
   TextStyle myWhiteTextStyle = TextStyle(color: Colors.white);
+  TextStyle myTextStyleH2 = TextStyle(
+    color: Colors.deepOrange[400],
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
 
-  ShapeBorder curveButton = RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0));
+  ShapeBorder curveButton =
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0));
 
   MyStyle();
-  
 }
